b8d937a6c253acdebb9b5aacc8df6ad47d047bd5
Jason <jason974448@gmail.com>
2025-02-19 11:05:06 -0400

    Renamed almost everything, and updated the MainMenu script
    I started the script last night but it was 1:30, and i forgot to push it.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text levelText; // UI Text to display the selected level
+    private string[] levels = { "Level1", "Level2", "Level3", "Level4" }; // Each level in order
+    private int currentLevelIndex = 0;
+
     void Start()
     {
-        
+        UpdateLevelText();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangeLevel(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangeLevel(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+
+    void ChangeLevel(int change)
+    {
+        currentLevelIndex += change;
+        if (currentLevelIndex >= levels.Length) currentLevelIndex = 0;
+        if (currentLevelIndex < 0) currentLevelIndex = levels.Length - 1;
+        UpdateLevelText();
+    }
+
+    void UpdateLevelText()
+    {
+        levelText.text = "Selected Level: " + levels[currentLevelIndex];
+
+        // Later use currentLevelIndex to change text to be more specific
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(levels[currentLevelIndex]);
     }
 }
